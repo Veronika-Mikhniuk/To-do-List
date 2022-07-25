48c1fb59bbc21f0e82d14c5986e78c674d3e2415
Veronika-Mikhnyuk <veronika.mikhnuyk@gmail.com>
2022-07-25 17:33:10 +0300

    Implementation of interface when To do List change

--- Models/To do Model.cs
+++ Models/To do Model.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace To_do_List.Models
 {
-	class To_do_Model
+	class To_do_Model: INotifyPropertyChanged //реализация интерфейса и события ниже, которые будут происходить при изменеии Списка
 	{
 		public DateTime CreationDate { get; set; } = DateTime.Now;//когда будет создваться запись, бужет создаваться даьа создания автматически
 
 		private bool _taskIsDone;//переменная типа bool потому что задача либо выполнена либо нет
 		private string _taskText; //перемеая для текста задачи
 
-		public bool taskIsDone //свойство для поля со статусом выпонения задачи
+        public bool taskIsDone //свойство для поля со статусом выпонения задачи
 		{
 			get { return _taskIsDone; }
 			set { _taskIsDone = value; }
@@ -23,5 +24,7 @@ namespace To_do_List.Models
 			get { return _taskText; }
 			set { _taskText = value; }
 		}
+
+		public event PropertyChangedEventHandler PropertyChanged;
 	}
 }
